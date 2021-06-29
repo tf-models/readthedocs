71d7f905fccca70bba35f548bb7b4c4b17b3815f
evawyf <evawyf1@gmail.com>
2021-06-29 08:00:52 -0700

    test

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# from sphinx.highlighting import lexers
+# from pygments.lexers.web import PhpLexer
+# lexers['php'] = PhpLexer(startinline=True, linenos=1)
+# lexers['php-annotations'] = PhpLexer(startinline=True, linenos=1)
+# primary_domain = 'php'
 
 # -- Project information -----------------------------------------------------
 
@@ -30,7 +35,11 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = [
+    'sphinx_rtd_theme',
+]
+
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +55,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/index.rst
+++ docs/index.rst
@@ -1,14 +1,11 @@
-Welcome to TensorFlow Model Garden's documentation!
-=======================================
+
+Welcome to Test's documentation!
+================================
+
+Contents:
 
 .. toctree::
    :maxdepth: 2
-   :caption: Get Started
 
-   get_started.md 
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`search`
+   overview
+   quickstart
